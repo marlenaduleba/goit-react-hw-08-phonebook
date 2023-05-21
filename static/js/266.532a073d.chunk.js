59594f690791ea1bfb77fac1559f90dc5a6e9763
marlenaduleba <110857031+marlenaduleba@users.noreply.github.com>
2023-05-21 12:47:38 +0000

    Deploying to gh-pages from @ marlenaduleba/goit-react-hw-08-phonebook@cdaf3f12e237edcfa8c44bb07ba7837bb8b01615 🚀

--- static/js/266.532a073d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var c=n(3329),i={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeigth:500,fontSize:48,textAlign:"center"}};function r(){return(0,c.jsx)("div",{style:i.container,children:(0,c.jsx)("h1",{style:i.title,children:"Contacts app welcome page \ud83d\udc81\u200d\u2640\ufe0f"})})}}}]);
-//# sourceMappingURL=266.532a073d.chunk.js.map
---
+++ static/js/415.aa8b4f4a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var c=n(3329),i={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeigth:500,fontSize:48,textAlign:"center"}};function r(){return(0,c.jsx)("div",{style:i.container,children:(0,c.jsx)("h1",{style:i.title,children:"Contacts app welcome page \ud83d\udc81\u200d\u2640\ufe0f"})})}}}]);
+//# sourceMappingURL=415.aa8b4f4a.chunk.js.map
