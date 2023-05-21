351d3b9080d734e75768cac4a40f3a910daaff09
Marlena Dulęba <marlena_duleba@o2.pl>
2023-05-21 12:20:45 +0200

    Section margin fixed

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,7 +4,7 @@ import { Typography, Stack } from '@mui/material';
 
 export const Section = ({ title, children }) => {
   return (
-    <Stack component="section"  sx={{ margin: '0 auto', maxWidth: '50ch'}} >
+    <Stack component="section"  sx={{ margin: '0 auto 32px', maxWidth: '50ch'}} >
       <Typography variant="h4" component="h2" align='center' sx={{marginBottom: 3}}>
   {title}
 </Typography>
