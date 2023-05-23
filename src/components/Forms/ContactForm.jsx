847cbd02d30a9aab18208f3c7daa1c5f74bf8506
Marlena Dulęba <marlena_duleba@o2.pl>
2023-05-23 10:29:05 +0200

    previous version of forms code restored

--- src/components/Forms/ContactForm.jsx
+++ src/components/Forms/ContactForm.jsx
@@ -1,13 +1,78 @@
+// import { useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { addContact } from 'redux/contacts/operations.js';
+// import { selectError } from 'redux/contacts/selectors.js';
+// import { selectAllContacts } from 'redux/contacts/selectors.js';
+// import { toast } from 'react-toastify';
+// import { Stack } from '@mui/material';
+// import { StyledButton } from 'styles/styles.js';
+// import { NameField } from './NameField.jsx';
+// import { NumberField } from './NumberField.jsx';
+
+// const initialValues = {
+//   name: '',
+//   number: '',
+// };
+
+// export const ContactForm = () => {
+//   const dispatch = useDispatch();
+//   const contacts = useSelector(selectAllContacts);
+//   const error = useSelector(selectError);
+//   const [values, setValues] = useState(initialValues);
+
+//   const handleFormSubmit = e => {
+//     e.preventDefault();
+
+//     if (nameUnique(values.name)) {
+//       dispatch(addContact(values));
+//       toast.success(`${values.name} added to your contacts.`);
+//       setValues(initialValues);
+//     }
+//   };
+
+//   const nameUnique = newName => {
+//     const searchUnique = newName.toLowerCase();
+
+//     if (contacts.find(({ name }) => name.toLowerCase() === searchUnique)) {
+//       toast.error(`${newName} is already in contacts.`);
+//       return false;
+//     }
+//     return true;
+//   };
+
+//   const handleInputChange = (fieldName, value) => {
+//     setValues(prevValues => ({ ...prevValues, [fieldName]: value }));
+//   };
+
+//   if (error) {
+//     toast.error(`ERROR`);
+//     return null;
+//   }
+
+//   return (
+//     <Stack
+//       sx={{ alignItems: 'center' }}
+//       component="form"
+//       onSubmit={handleFormSubmit}
+//       autoComplete="off"
+//     >
+//       <NameField value={values.name} onInputChange={handleInputChange} />
+//       <NumberField value={values.number} onInputChange={handleInputChange} />
+//       <StyledButton type="submit">Add Contact</StyledButton>
+//     </Stack>
+//   );
+// };
+
+
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/contacts/operations.js';
 import { selectError } from 'redux/contacts/selectors.js';
 import { selectAllContacts } from 'redux/contacts/selectors.js';
 import { toast } from 'react-toastify';
-import { Stack } from '@mui/material';
-import { StyledButton } from 'styles/styles.js';
-import { NameField } from './NameField.jsx';
-import { NumberField } from './NumberField.jsx';
+import { Input, InputLabel, Stack } from '@mui/material';
+import { StyledButton, StyledFormControl } from 'styles/styles.js';
+
 
 const initialValues = {
   name: '',
@@ -18,14 +83,14 @@ export const ContactForm = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectAllContacts);
   const error = useSelector(selectError);
-  const [values, setValues] = useState(initialValues);
+  const [{name, number}, setValues] = useState(initialValues);
 
   const handleFormSubmit = e => {
     e.preventDefault();
 
-    if (nameUnique(values.name)) {
-      dispatch(addContact(values));
-      toast.success(`${values.name} added to your contacts.`);
+    if (nameUnique(name)) {
+      dispatch(addContact({name, number}));
+      toast.success(`${name} added to your contacts.`);
       setValues(initialValues);
     }
   };
@@ -40,10 +105,11 @@ export const ContactForm = () => {
     return true;
   };
 
-  const handleInputChange = (fieldName, value) => {
-    setValues(prevValues => ({ ...prevValues, [fieldName]: value }));
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setValues(prevValues => ({ ...prevValues, [name]: value }));
   };
-
+  
   if (error) {
     toast.error(`ERROR`);
     return null;
@@ -56,9 +122,40 @@ export const ContactForm = () => {
       onSubmit={handleFormSubmit}
       autoComplete="off"
     >
-      <NameField value={values.name} onInputChange={handleInputChange} />
-      <NumberField value={values.number} onInputChange={handleInputChange} />
+      <StyledFormControl required autoComplete="off" fullWidth>
+      <InputLabel htmlFor="standard-adornment-name">Name</InputLabel>
+      <Input
+        id="standard-adornment-name"
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleInputChange}
+        autoComplete="off"
+        inputProps={{
+          pattern: "[A-Za-zĄąĆćĘęŁłŃńÓóŚśŹźŻż\\-' ]+",
+          title:
+            'Only letters, apostrophe, dash, spaces, and Polish characters are allowed',
+        }}
+      />
+    </StyledFormControl>
+    <StyledFormControl required autoComplete="off" fullWidth>
+      <InputLabel htmlFor="standard-adornment-number">Number</InputLabel>
+      <Input
+        id="standard-adornment-number"
+        type="tel"
+        name="number"
+        value={number}
+        onChange={handleInputChange}
+        autoComplete="off"
+        inputProps={{
+          pattern: "^[+]?[0-9 \\u0028\\u0029\\u002D]*$",
+          title: "Phone number must consist of digits and may include spaces, dashes, and parentheses. It can start with '+'",
+        }}
+      />
+    </StyledFormControl>
       <StyledButton type="submit">Add Contact</StyledButton>
     </Stack>
   );
 };
+
+
--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -1,18 +1,71 @@
+// import { logIn } from 'redux/auth/operations.js';
+// import { useDispatch } from 'react-redux';
+// import { StyledButton } from 'styles/styles.js';
+// import { Stack } from '@mui/material';
+
+
+// import { EmailField } from 'components/Forms/EmailField.jsx';
+// import { PasswordField } from 'components/Forms/PasswordField.jsx';
+
+// import {
+//   useHandleSubmit,
+//   useHandleInputChange,
+//   useHandleClickShowPassword,
+//   useHandleMouseDownPassword,
+// } from 'hooks/useFormUtils.js';
+
+// const initialValues = {
+//   email: '',
+//   password: '',
+//   showPassword: false,
+// };
+
+// export default function LoginForm() {
+//   const dispatch = useDispatch();
+
+//   const [values, handleSubmit, setValues] = useHandleSubmit(
+//     initialValues,
+//     values => dispatch(logIn(values))
+//   );
+
+//   const handleInputChange = useHandleInputChange(setValues);
+//   const handleClickShowPassword = useHandleClickShowPassword(setValues);
+//   const handleMouseDownPassword = useHandleMouseDownPassword(setValues);
+
+//   const handleEmailChange = e => {
+//     handleInputChange(e, setValues);
+//   };
+
+//   const handlePasswordChange = e => {
+//     handleInputChange(e, setValues);
+//   };
+
+//   return (
+//     <Stack
+//       sx={{ alignItems: 'center' }}
+//       component="form"
+//       onSubmit={handleSubmit}
+//       autoComplete="off"
+//     >
+//       <EmailField value={values.email} onChange={handleEmailChange} />
+//       <PasswordField
+//         value={values.password}
+//         onChange={handlePasswordChange}
+//         showPassword={values.showPassword}
+//         onClickToggle={handleClickShowPassword}
+//         onMouseDown={handleMouseDownPassword}
+//       />
+//       <StyledButton type="submit">Log In</StyledButton>
+//     </Stack>
+//   );
+// }
+
 import { logIn } from 'redux/auth/operations.js';
 import { useDispatch } from 'react-redux';
-import { StyledButton } from 'styles/styles.js';
-import { Stack } from '@mui/material';
-
-
-import { EmailField } from 'components/Forms/EmailField.jsx';
-import { PasswordField } from 'components/Forms/PasswordField.jsx';
-
-import {
-  useHandleSubmit,
-  useHandleInputChange,
-  useHandleClickShowPassword,
-  useHandleMouseDownPassword,
-} from 'hooks/useFormUtils.js';
+import { useState } from 'react';
+import { StyledButton, StyledFormControl} from 'styles/styles.js';
+import { IconButton, Input, InputAdornment, InputLabel, Stack } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 const initialValues = {
   email: '',
@@ -20,42 +73,77 @@ const initialValues = {
   showPassword: false,
 };
 
-export default function LoginForm() {
+
+
+export const LoginForm = () => {
   const dispatch = useDispatch();
+  const [values, setValues] = useState(initialValues);
 
-  const [values, handleSubmit, setValues] = useHandleSubmit(
-    initialValues,
-    values => dispatch(logIn(values))
-  );
-
-  const handleInputChange = useHandleInputChange(setValues);
-  const handleClickShowPassword = useHandleClickShowPassword(setValues);
-  const handleMouseDownPassword = useHandleMouseDownPassword(setValues);
-
-  const handleEmailChange = e => {
-    handleInputChange(e, setValues);
+  const handleSubmit = e => {
+    const { email, password } = values;
+    e.preventDefault();
+    dispatch(logIn({ email, password }));
+    setValues(initialValues);
   };
 
-  const handlePasswordChange = e => {
-    handleInputChange(e, setValues);
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setValues(prevValues => ({ ...prevValues, [name]: value }));
+  };
+
+  const handleClickShowPassword = () => {
+    setValues(prevValues => ({
+      ...prevValues,
+      showPassword: !prevValues.showPassword,
+    }));
+  };
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
   };
 
   return (
-    <Stack
-      sx={{ alignItems: 'center' }}
-      component="form"
-      onSubmit={handleSubmit}
+        <Stack
+          sx={{ alignItems: 'center' }}
+          component="form"
+          onSubmit={handleSubmit}
+          autoComplete="off"
+        >
+          <StyledFormControl required autoComplete="off" fullWidth>
+    <InputLabel htmlFor="standard-adornment-email">Email</InputLabel>
+    <Input
+      id="standard-adornment-email"
+      type="email"
+      name="email"
+      value={values.email}
+      onChange={handleInputChange}
       autoComplete="off"
-    >
-      <EmailField value={values.email} onChange={handleEmailChange} />
-      <PasswordField
+      
+    />
+  </StyledFormControl>
+  <StyledFormControl required autoComplete="off" fullWidth>
+      <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+      <Input
+        id="standard-adornment-password"
+        type={values.showPassword ? 'text' : 'password'}
+        name="password"
         value={values.password}
-        onChange={handlePasswordChange}
-        showPassword={values.showPassword}
-        onClickToggle={handleClickShowPassword}
-        onMouseDown={handleMouseDownPassword}
+        onChange={handleInputChange}
+        autoComplete="off"
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+            >
+              {values.showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        }
       />
-      <StyledButton type="submit">Log In</StyledButton>
-    </Stack>
-  );
-}
+    </StyledFormControl>
+          <StyledButton type="submit">Log In</StyledButton>
+        </Stack>
+      );
+};
